1f77fe3ffa187501f3b3a439373b5b7f868b4a73
Stepan Leonov <s.leonov@innopolis.university>
2024-06-30 10:34:56 +0300

    Router init and after build clean-up

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HelloWorld from './views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HelloWorld,
+    },
+  ]
+})
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
