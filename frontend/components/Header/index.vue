6500da4b526fdbf5d036744ff9013e24ffff1dff
Ekaterina Zaitseva <katy.zaitsewa2207@ya.ru>
2024-07-09 20:47:24 +0300

    Navbar changes

--- frontend/components/Header/index.vue
+++ frontend/components/Header/index.vue
@@ -1,29 +1,32 @@
 <template>
-    <div class="header flex items-center h-15 justify-between p-6 bg-white bg-opacity-20 absolute top-0 left-0 w-screen z-10">
-        <HeaderLogo />
-        <ul class="flex gap-4 font-ibm">
-            <li>
-                <NuxtLink class="hover:text-slate-800 font-ibm" to="/">Home</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink class="hover:text-slate-800 font-ibm" to="/projects">Projects</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink class="hover:text-slate-800 font-ibm" to="/volunteer">Volunteer</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink class="hover:text-slate-800 font-ibm" to="/user">User Info</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink class="hover:text-slate-800 font-ibm" to="/offers">Offers</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink class="hover:text-slate-800 font-ibm" to="/donate">Donate</NuxtLink>
-            </li>
-        </ul>
-    </div>
-</template>
+<div class="header flex items-center h-15 justify-between p-6 bg-white bg-opacity-20 absolute top-0 left-0 w-screen z-10">
+    <HeaderLogo />
+    <ul class="flex gap-4 font-ibm">
+    <li>
+        <NuxtLink class="hover:text-[#40BA21] font-ibm" to="/">Home</NuxtLink>
+    </li>
+    <li>
+        <NuxtLink class="hover:text-[#40BA21] font-ibm" to="/projects">Projects</NuxtLink>
+    </li>
+    <li>
+        <NuxtLink class="hover:text-[#40BA21] font-ibm" to="/volunteer">Volunteer</NuxtLink>
+    </li>
+    <li>
+        <NuxtLink class="hover:text-[#40BA21] font-ibm" to="/user">User Info</NuxtLink>
+    </li>
+    <li>
+        <NuxtLink class="hover:text-[#40BA21] font-ibm" to="/offers">Offers</NuxtLink>
+    </li>
+    <li>
+        <NuxtLink class="hover:text-[#40BA21] font-ibm" to="/donate">Donate</NuxtLink>
+    </li>
+    <li>
+        <NuxtLink class="hover:text-[#40BA21] font-ibm" to="/sign-in">Sign In</NuxtLink>
+    </li>
+    </ul>
 
+</div>
+</template>
 <script setup>
 
 </script>
@@ -31,4 +34,5 @@
 <style lang="sass" scoped>
 .router-link-exact-active
     font-weight: bold
+    color: #40BA21
 </style>
