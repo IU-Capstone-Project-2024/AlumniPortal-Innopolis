cac168c903203b8b5c6bf459598e93c9729493ef
Ekaterina Zaitseva <katy.zaitsewa2207@ya.ru>
2024-07-12 00:01:17 +0300

    Style of sign-in form + sign-up page + header for sign-up page

--- frontend/components/Header/index.vue
+++ frontend/components/Header/index.vue
@@ -1,24 +1,24 @@
 <template>
-    <div :class="route.name == 'sign-in' ? 'bg-opacity-75 dark:bg-opacity-75' : 'bg-opacity-20 dark:bg-opacity-20'"
+    <div :class="route.name == 'sign-in' || route.name =='sign-up' ? 'bg-opacity-75 dark:bg-opacity-75' : 'bg-opacity-20 dark:bg-opacity-20'"
         class="header flex items-center h-15 justify-between p-6 bg-white dark:bg-black absolute top-0 left-0 w-screen z-10">
         <HeaderLogo />
         <ul class="flex gap-4 font-ibm">
             <li>
                 <NuxtLink class="hover:text-[#40BA21] font-ibm dark:text-white" to="/">Home</NuxtLink>
             </li>
-            <li v-if="route.name != 'sign-in'">
+            <li v-if="route.name != 'sign-in' && route.name != 'sign-up'">
                 <NuxtLink class="hover:text-[#40BA21] font-ibm dark:text-white" to="/projects">Projects</NuxtLink>
             </li>
-            <li v-if="route.name != 'sign-in'">
+            <li v-if="route.name != 'sign-in' && route.name != 'sign-up'">
                 <NuxtLink class="hover:text-[#40BA21] font-ibm dark:text-white" to="/volunteer">Volunteer</NuxtLink>
             </li>
-            <li v-if="route.name != 'sign-in'">
+            <li v-if="route.name != 'sign-in' && route.name != 'sign-up'">
                 <NuxtLink class="hover:text-[#40BA21] font-ibm dark:text-white" to="/user">User Info</NuxtLink>
             </li>
-            <li v-if="route.name != 'sign-in'">
+            <li v-if="route.name != 'sign-in' && route.name != 'sign-up'">
                 <NuxtLink class="hover:text-[#40BA21] font-ibm dark:text-white" to="/offers">Offers</NuxtLink>
             </li>
-            <li v-if="route.name != 'sign-in'">
+            <li v-if="route.name != 'sign-in' && route.name != 'sign-up'">
                 <NuxtLink class="hover:text-[#40BA21] font-ibm dark:text-white" to="/donate">Donate</NuxtLink>
             </li>
             <li>
--- frontend/pages/sign-up/index.vue
+++ frontend/pages/sign-up/index.vue
@@ -2,7 +2,7 @@
   <div class="bg-[url(~/assets/inno.png)] bg-cover w-screen h-screen flex items-center justify-center">
     <div class="wrapper bg-white p-10 rounded-lg shadow-lg">
       <form @submit.prevent="handleSubmit" class="space-y-6">
-        <h2 class="text-3xl font-bold text-center" style="color: #40BA21">Sign in</h2>
+        <h2 class="text-3xl font-bold text-center" style="color: #40BA21">Sign up</h2>
         <FormKit 
           type="text" 
           name="login" 
@@ -24,19 +24,12 @@
           label-class="block text-sm font-medium text-gray-700"
         />
         <button 
-          type="submit" 
-          class="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-green-700; focus:outline-none; focus:ring-2; focus:ring-offset-2; focus:ring-green-500"
-          style="background-color: #40BA21"
-        >
-          Sign in
-        </button>
-        <button 
           type="button" 
-          @click="navigateToSignUp" 
+          @click="navigateToSignIn" 
           class="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-green-700; focus:outline-none; focus:ring-2; focus:ring-offset-2; focus:ring-green-500"
           style="background-color: #40BA21"
         >
-          Register
+          Sign Up
         </button>
       </form>
     </div>
@@ -52,8 +45,8 @@ const handleSubmit = () => {
   // handle form submission logic here
 }
 
-const navigateToSignUp = () => {
-  router.push('/sign-up')
+const navigateToSignIn = () => {
+  router.push('/sign-in')
 }
 </script>
 
