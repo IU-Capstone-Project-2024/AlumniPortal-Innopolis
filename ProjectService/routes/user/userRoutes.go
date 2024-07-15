751eb36106a25e839536244f852ee90d544db618
iucdcs1 <130791096+iucdcs1@users.noreply.github.com>
2024-07-15 03:05:48 +0300

    Update userRoutes.go

--- ProjectService/routes/user/userRoutes.go
+++ ProjectService/routes/user/userRoutes.go
@@ -10,10 +10,14 @@ func SetupRouter(route *gin.Engine) {
 	student := route.Group("/projects")
 	student.Use(middleware.RequireStudent)
 	{
-		student.GET("", controllers.GetCurrentUserProjects)
+		student.GET("/user", controllers.GetCurrentUserProjects)
 		student.GET("/:id", controllers.GetProject)
 		student.PATCH("/:id/edit", controllers.UpdateProject)
 		student.DELETE("/:id/delete", controllers.DeleteProject)
 		student.POST("/create", controllers.CreateProject)
+	}
+	student.Use(middleware.RequireVerify)
+	{
+		student.GET("", controllers.GetProjects)
 	}
 }
