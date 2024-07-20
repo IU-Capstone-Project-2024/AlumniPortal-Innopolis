8354aa169262e289356f6ab62c501e8d3e03e1cf
iucdcs1 <130791096+iucdcs1@users.noreply.github.com>
2024-07-20 23:36:32 +0300

    Update main.go

--- AuthService/main.go
+++ AuthService/main.go
@@ -37,34 +37,28 @@ func main() {
 
 	logrus.Info("Starting Auth Service")
 
-	go func() {
-		certFile := "certs/selfsigned.crt"
-		keyFile := "certs/selfsigned.key"
+	certFile := "certs/selfsigned.crt"
+	keyFile := "certs/selfsigned.key"
 
-		httpsServer := &http.Server{
-			Addr:    ":8081",
-			Handler: r,
-		}
+	httpsServer := &http.Server{
+		Addr:    ":8081",
+		Handler: r,
+	}
 
-		if err := httpsServer.ListenAndServeTLS(certFile, keyFile); err != nil && err != http.ErrServerClosed {
-			logrus.Fatal("Failed to start HTTPS Auth Service:", err)
-			return
-		}
-		logrus.Info("HTTPS Auth Service started")
-	}()
+	if err := httpsServer.ListenAndServeTLS(certFile, keyFile); err != nil && err != http.ErrServerClosed {
+		logrus.Fatal("Failed to start HTTPS Auth Service:", err)
+		return
+	}
+	logrus.Info("HTTPS Auth Service started")
 
-	go func() {
-		httpPort := ":8091"
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			logrus.Infof("Redirecting request from %s to https://%s:8081%s", r.Host, r.Host, r.RequestURI)
-			http.Redirect(w, r, "https://"+r.Host+":8081"+r.RequestURI, http.StatusMovedPermanently)
-		})
-		if err := http.ListenAndServe(httpPort, nil); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			logrus.Fatal("Failed to start HTTP server for Auth Service:", err)
-			panic(err)
-		}
-		logrus.Info("HTTP Auth Service started")
-	}()
-
-	logrus.Info("Check")
+	httpPort := ":8091"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		logrus.Infof("Redirecting request from %s to https://%s:8081%s", r.Host, r.Host, r.RequestURI)
+		http.Redirect(w, r, "https://"+r.Host+":8081"+r.RequestURI, http.StatusMovedPermanently)
+	})
+	if err := http.ListenAndServe(httpPort, nil); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		logrus.Fatal("Failed to start HTTP server for Auth Service:", err)
+		panic(err)
+	}
+	logrus.Info("HTTP Auth Service started")
 }
